3a64cc0354b372a2b3466ee094dade40cfafacc8
MSteplight <hesteps.jr@gmail.com>
2024-10-23 15:47:21 -0400

    I3 trade.py

--- trade.py
+++ trade.py
@@ -1,5 +1,3 @@
-''' THIS WILL BE USED WITH EACH STOCK'S OWN BUY AND SELL METHODS'''
-
 import alpaca_trade_api as tradeapi
 import pandas as pd
 import numpy as np
@@ -13,17 +11,7 @@ from PIL import Image, ImageTk
 import tkinter.ttk as ttk
 import time
 
-# import alpaca_trade_api as tradeapi
-
-'''
-import numpy as np
-import pandas as pd
-import yfinance as yf
-import matplotlib.pyplot as plt
-'''
-
-
-BASE_URL = 'https://paper-api.alpaca.markets'
+BASE_URL = 'https://paper-api.alpaca.markets/v2'
 ACCOUNT_URL = '{}/v2/account'.format(BASE_URL)
 ORDERS_URL = '{}/v2/orders'.format(BASE_URL)
 ALL_ORDERS = '{}?status=all'.format(ORDERS_URL)
@@ -47,70 +35,6 @@ METATRADE_PRICE = 'https://data.alpaca.markets/v2/stocks/META/trades/latest?feed
 GOOGTRADE_PRICE = 'https://data.alpaca.markets/v2/stocks/GOOG/trades/latest?feed=iex'
 HEADERS = {'APCA-API-KEY-ID': API_KEY,'APCA-API-SECRET-KEY': SECRET_KEY}
 
-TK_SILENCE_DEPRECATION=1
-
-api = tradeapi.REST(API_KEY, SECRET_KEY, BASE_URL, api_version='v2')
-
-def get_account():
-     r = requests.get(ACCOUNT_URL,headers=HEADERS)
-     return json.loads(r.content)
-
-def get_cash():
-
-     r = requests.get(ACCOUNT_URL, headers=HEADERS)
-     response = json.loads(r.content)
-     cash = response['cash']
-     #print(f'Your cash amount is: ${cash}')
-     return cash
-
-
-def get_alpaca_data(ticker, timeframe='1D', start_date=None, end_date=None, limit=100): # New method to access real time data
-    barset = api.get_bars(ticker, timeframe, start=start_date, end=end_date, limit=limit).df
-    return barset
-
-
-def bollinger_bands(df, window=20, no_of_std=2):
-    df['SMA'] = df['close'].rolling(window=window).mean() # finding the Simple Moving Average
-    df['STD'] = df['close'].rolling(window=window).std() # Calculating Standard deviation
-    df['Upper Band'] = df['SMA'] + (no_of_std * df['STD'])  # Applying SD to upper band to get sell value
-    df['Lower Band'] = df['SMA'] - (no_of_std * df['STD'])  # Applyinng SD to lower band to get buy value
-    return df
-
-
-def bollinger_strategy(df): # Our Trend Trading Strategy 
-    buy_signal = []
-    sell_signal = []
-
-    for i in range(len(df)):
-        if df['close'][i] < df['Lower Band'][i]:  # Buy condition
-            buy_signal.append(df['close'][i])
-            sell_signal.append(np.nan)
-        elif df['close'][i] > df['Upper Band'][i]:  # Sell condition
-            sell_signal.append(df['close'][i])
-            buy_signal.append(np.nan)
-        else:
-            buy_signal.append(np.nan)
-            sell_signal.append(np.nan)
-
-    df['Buy Signal'] = buy_signal
-    df['Sell Signal'] = sell_signal
-    return df
-
-
-# Example usage
-ticker = 'AAPL'
-start_date = '2022-01-01'
-end_date = '2023-01-01'
-timeframe = '1Day'  # You can adjust the timeframe
-
-# Fetch data from Alpaca
-df = get_alpaca_data(ticker, timeframe=timeframe, start_date=start_date, end_date=end_date)
-
-# Calculate Bollinger Bands
-df = bollinger_bands(df)
-
-# Apply the strategy
-df = bollinger_strategy(df)
 
 
 def get_tradePrice(stock):
@@ -120,6 +44,37 @@ def get_tradePrice(stock):
 
      return price
 
+def get_cash():
+
+     r = requests.get(ACCOUNT_URL, headers=HEADERS)
+     response = json.loads(r.content)
+     cash = response['cash']
+     #print(f'Your cash amount is: ${cash}')
+     return cash
+
+def is_trading_hours():
+    current_time = time.localtime()
+    if current_time.tm_hour >= 9 and current_time.tm_hour < 16:
+        return True
+    else:
+        return False
+    
+def sell_all_shares(stock):
+     if stock == 'AAPL':
+        URL = CLOSEPOSAAPL_URL
+     if stock == 'TSLA':
+          URL = CLOSEPOSTSLA_URL
+     if stock == 'AMZN':
+          URL = CLOSEPOSAMZN_URL
+     if stock == 'GOOG':
+          URL = CLOSEPOSGOOG_URL
+     if stock == 'META':
+          URL = CLOSEPOSMETA_URL
+     if stock == 'MSFT':
+          URL = CLOSEPOSMSFT_URL
+
+     r = requests.delete(URL, headers=HEADERS)
+     print(r.text)
 
 # Function to create an order
 def create_order(symbol, qty, side, type, time_in_force):
@@ -135,98 +90,6 @@ def create_order(symbol, qty, side, type, time_in_force):
     print(response)
 
 
-def get_orders():
-     r = requests.get(ALL_ORDERS, headers=HEADERS)
-     return json.loads(r.content)
-
-# Function to check if the current time is within trading hours
-def is_trading_hours():
-    current_time = time.localtime()
-    if current_time.tm_hour >= 9 and current_time.tm_hour < 16:
-        return True
-    else:
-        return False
-
-# Function to activate the bot at market opening
-def activate_bot():
-    if is_trading_hours():
-         # Make purchases of specified stocks
-        # buy_stocks()
-         monitor_stock_pl()
-         #schedule_sell_before_market_close()
-    else:
-         print("Market is closed. Cannot activate bot.")
-
-# Function to deactivate the bot at market closing
-def deactivate_bot():
-    if is_trading_hours():
-        # Sell all stocks before market closes
-        sell_all_stocks()
-    else:
-        print("Market is closed. Bot is already deactivated.")
-
-
-
-'''
-# Function to buy specified stocks
-def buy_stocks():
-    stocks_to_buy = ['AAPL', 'TSLA', 'AMZN', 'GOOG', 'META', 'MSFT']
-    for stock in stocks_to_buy:
-        #last_trade = api.get_last_trade(stock)
-        create_order(symbol=stock, qty=1, side='buy', type='market', time_in_force='day') 
-
-def sell_stocks_if_price_change():
-    orders = get_orders()
-    current_time = time.localtime()
-    for order in orders:
-        symbol = order['symbol']
-        # Retrieve current price of the stock
-        current_price = get_current_price(symbol)
-        # Calculate percentage change
-        purchase_price = get_purchase_price(symbol)
-        percentage_change = ((current_price - purchase_price) / purchase_price) * 100
-        if abs(percentage_change) >= 0.5:  # If price change is >= 0.5% in either direction
-            create_order(symbol=symbol, qty=order['qty'], side='sell', type='market', time_in_force='day')
-        elif current_time.tm_hour == 10 and current_time.tm_min == 1:
-            create_order(symbol=symbol, qty=order['qty'], side='sell', type='market', time_in_force='day')
-        deactivate_bot()
-'''
-
-# Function to get the current price of a stock
-def get_current_price(symbol):
-    BASE_URL = 'https://data.alpaca.markets/v2/stocks/{}/quote'.format(symbol)
-    HEADERS = {'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY}
-    r = requests.get(BASE_URL, headers=HEADERS)
-    response = json.loads(r.content)
-    current_price = response['last']['price']
-    print(f'Current price is: {current_price}')
-    return current_price
-
-# Function to get the purchase price of a stock
-def get_purchase_price(symbol):
-    BASE_URL = 'https://paper-api.alpaca.markets/v2/orders'
-    HEADERS = {'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY}
-    r = requests.get(BASE_URL, headers=HEADERS)
-    orders = json.loads(r.content)
-    for order in orders:
-        if order['symbol'] == symbol and order['side'] == 'buy':
-            return order['filled_avg_price']
-    return None  # If no buy order found for the symbol
-
-# Function to schedule selling of stocks 15 minutes before market close
-'''
-# def schedule_sell_before_market_close():
-    while True:
-        Check if it is 15 minutes before market close
-        print('Checking Time')
-        current_time = time.localtime()
-        # 14 52
-        if current_time.tm_hour == 14 and current_time.tm_min == 52:
-            sell_all_stocks()
-            break
-        else:
-            time.sleep(60)  
-'''
 # Function to sell all stocks
 def sell_all_stocks():
     stocks_to_sell = ['AAPL', 'TSLA', 'AMZN', 'GOOG', 'META', 'MSFT']
@@ -235,7 +98,6 @@ def sell_all_stocks():
     # orders = get_orders()
     # for order in orders:
     #   create_order(symbol=order['symbol'], qty=order['qty'], side='sell', type='market', time_in_force='day')
-
 
 def profitLoss():
     r = requests.get(POSITIONS_URL, headers=HEADERS)
@@ -258,8 +120,6 @@ def profitLoss():
     return floatsList
 
 
-    
-# Function to monitor stock prices and adjust strategies
 def monitor_stock_pl():
     if is_trading_hours():
         print('Monitoring Profit/Loss')
@@ -286,36 +146,6 @@ def monitor_stock_pl():
         pass
     
 
-def sell_all_shares(stock):
-     if stock == 'AAPL':
-        URL = CLOSEPOSAAPL_URL
-     if stock == 'TSLA':
-          URL = CLOSEPOSTSLA_URL
-     if stock == 'AMZN':
-          URL = CLOSEPOSAMZN_URL
-     if stock == 'GOOG':
-          URL = CLOSEPOSGOOG_URL
-     if stock == 'META':
-          URL = CLOSEPOSMETA_URL
-     if stock == 'MSFT':
-          URL = CLOSEPOSMSFT_URL
-
-     r = requests.delete(URL, headers=HEADERS)
-     print(r.text)
-
-
-
-    
-    
-def monitor_stock_price():
-    if is_trading_hours():
-        print('It is within trading hours, monitoring stock prices...')
-        stocks = ['AAPL', 'TSLA', 'AMZN', 'GOOG', 'META', 'MSFT']
-        for stock in stocks:
-            pass
-
-
-
 def get_closedorders():
      r = requests.get(CLOSED_ORDERS, headers=HEADERS)
      return json.loads(r.content)
@@ -324,6 +154,27 @@ def get_neworders():
     r = requests.get(NEW_ORDERS, headers=HEADERS)
     return json.loads(r.content)
 
+# Function to activate the bot at market opening
+def activate_bot():
+    if is_trading_hours():
+         # Make purchases of specified stocks
+        # buy_stocks()
+         monitor_stock_pl()
+         #schedule_sell_before_market_close()
+    else:
+         print("Market is closed. Cannot activate bot.")
+
+# Function to deactivate the bot at market closing
+def deactivate_bot():
+    if is_trading_hours():
+        # Sell all stocks before market closes
+        sell_all_stocks()
+    else:
+        print("Market is closed. Bot is already deactivated.")
+
+
+
+# UI
 
 def mainWindow():
      welcome.destroy()
@@ -565,106 +416,3 @@ profitLoss()
 
 welcome.mainloop()
 #root.mainloop()
-
-
-
-
-'''
-def price_increased(current_price, previous_price):
-    return current_price > previous_price
-
-def price_decreased(current_price, previous_price):
-    return current_price < previous_price
-
-previous_price = {}
-'''
-
-'''
-# Function to sell all stocks
-def sell_all_stocks():
-    orders = get_orders()
-    current_time = time.localtime()
-    for order in orders:
-        symbol = order['symbol']
-        price = order['price']
-        if symbol not in previous_price:
-            previous_price[symbol] = price
-            continue
-
-        price_change = (price - previous_price[symbol]) / previous_price[symbol] * 100
-        if price_change >= 1:
-            create_order(symbol=order['symbol'], qty=order['qty'], side='sell', type='market', time_in_force='day')
-            previous_price[symbol] = price
-        elif price_change <= -1:
-            create_order(symbol=order['symbol'], qty=order['qty'], side='sell', type='market', time_in_force='day')
-            previous_price[symbol] = price
-        elif current_time.tm_hour == 15  and current_time.tm_min == 55:
-            create_order(symbol=order['symbol'], qty=order['qty'], side='sell', type='market', time_in_force='day')
-        deactivate_bot()
-'''
-'''
-def profitLoss():
-    r = requests.get(POSITIONS_URL, headers=HEADERS)
-    response = json.loads(r.content)
-    stocks = ['AAPL', 'TSLA', 'AMZN', 'GOOGL', 'META', 'MSFT']
-    # unrealized_plpc
-    for stock in stocks:
-        if response['symbol'] == stock:
-            print(response['unrealized_plpc'])
-            return response['unrealized_plpc']
-'''
-
-
-
-
-# DOESNT WORK YET
-# Function to monitor stock prices and adjust strategies
-'''def monitor_stock_pl():
-    while True:
-        if is_trading_hours():
-            print('Monitor stock pl method')
-            # Implement your monitoring and strategy adjustment logic here
-            # put the price that it was bought at and check the price that it is at now
-            # calculate the percentage
-            stocks = ['AAPL', 'TSLA', 'AMZN', 'GOOGL', 'META', 'MSFT']
-            for stock in stocks:
-                if profitLoss() > 0:
-                    print('Greater than 0')
-
-                else:
-                    print('Less than 0')
-
-
-        else:
-            break'''
-
-'''def monitor_stock_prices():
-    while True:
-        if is_trading_hours():
-            # Implement your monitoring and strategy adjustment logic here
-            # put the price that it was bought at and check the price that it is at now
-            # calculate the percentage
-            stocks = ['AAPL', 'TSLA', 'AMZN', 'GOOGL', 'META', 'MSFT']
-            for stock in stocks:
-                # 159.75
-                tradedPrice = get_tradePrice(stock)
-                time.sleep(60)
-                #159.74
-                currentPrice = get_tradePrice(stock)
-
-                priceChange = currentPrice - tradedPrice
-
-                if profitLoss > 0:
-                    print('Greater than 0')
-
-                else:
-                    print('Less than 0')
-
-
-        else:
-            break
-        time.sleep(60)'''
-
-
-
-
